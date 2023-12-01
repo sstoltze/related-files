ac53bb55d8f7d5e02e731febaf13911fb13ec6ad
Sarah Ella Stoltze <sstoltze@gmail.com>
2023-12-01 13:11:29 +0100

    Use projectile by default if available

--- related-files.el
+++ related-files.el
@@ -63,7 +63,8 @@
 (defun related-files--project-root ()
   "Get the root of the project, depending on what libraries are available."
   (expand-file-name
-   (or (and (fboundp 'project-root) (fboundp 'project-current) (project-root (project-current)))
+   (or (and (fboundp 'projectile-project-root) (projectile-project-root))
+       (and (fboundp 'project-root) (fboundp 'project-current) (project-root (project-current)))
        (and (fboundp 'vc-root-dir) (vc-root-dir))
        "~/")))
 
